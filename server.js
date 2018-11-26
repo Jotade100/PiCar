bbf1c28d7c47cd95bdc5ee8a65d4b5cabc45878d
Juan Diego <jotade100@gmail.com>
2018-11-26 09:52:33 -0600

    Actualización server
    Agregada la lectura y escritutura sobre estado del vehículo. Forma beta de encendido y apagado.

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 // Constantes de trabajo
 const express = require('express')
 const bodyParser = require('body-parser');
-var url = require('url');
 const querystring = require('querystring'); 
 var MongoClient = require('mongodb').MongoClient;
+var exec = require('child_process').exec;
 
 //// ------------------------------------------------------------------------------------
 var url = "mongodb://localhost:27017/";
@@ -23,16 +23,68 @@ app.get("/", function (req, res, error){
 		  if (err) throw err;
 		  dbo.collection("movimientos").find({}).toArray(function(err, resultados) {
 			if (err) throw err;
-			console.log(result)
-			console.log(resultados)
-			db.close();
+			dbo.collection("estado").find({}).toArray(function(err, resultado) {
+				if (err) throw err;
+				dbo.collection("encendido").find({}).toArray(function(err, results) {
+					if (err) throw err;
+					// sensores
+					console.log(result)
+					// movimientos
+					console.log(resultados)
+					// estado (corriendo o estacionado; velocidad)
+					console.log(resultado)
+					// encendido (si el carro está encendido)
+					console.log(results)
+					db.close();
+				});
+			});
 		  });
+		});
+	  });
 
+})
+
+// GET (métodos para el carro)
+app.get("/estado", function (req, res, error){
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("piCar");
+		dbo.collection("estado").find({}).toArray(function(err, result) {
+		  if (err) throw err;
+		  console.log(resultados)
 		  db.close();
 		});
 	  });
 
 })
+
+app.get("/encendido", function (req, res, error){
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("piCar");
+		dbo.collection("encendido").find({}).toArray(function(err, result) {
+		  if (err) throw err;
+		  console.log(resultados)
+		  if(resultados[resultados.lenght()-1].boton) {
+			child = exec("python miprograma.py", function (error, stdout, stderr) {
+				if (error !== null) {
+					console.log('exec error: ' + error);
+				}
+			});
+		  } else {
+			child = exec("^C", function (error, stdout, stderr) {
+				if (error !== null) {
+					console.log('exec error: ' + error);
+				}
+			});
+
+		  }
+		  db.close();
+		});
+	  });
+
+})
+
 
 // POST (para insertar sensores)
 app.post("/sensores", function(req, res) {
@@ -68,6 +120,36 @@ app.post("/movimiento", function(req, res) {
 	  });	
 });
 
+// POST (para comunicar el estado del carro desde la página)
+app.post("/estado", function(req, res) {
+	var movimiento = req.body.mov;
+	var velocidad = req.body.vel;
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("piCar");
+		var myobj = { estado: movimiento, velocidad: velocidad };
+		dbo.collection("estado").insertOne(myobj, function(err, res) {
+		  if (err) throw err;
+		  console.log("1 documento insertado");
+		  db.close();
+		});
+	  });	
+});
+
+app.post("/encendido", function(req, res) {
+	var boton = req.body.boton;
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("piCar");
+		var myobj = { boton: boton };
+		dbo.collection("encendido").insertOne(myobj, function(err, res) {
+		  if (err) throw err;
+		  console.log("1 documento insertado");
+		  db.close();
+		});
+	  });	
+});
+
 if (module === require.main) {
   // [START server]
   // Start the server
@@ -83,7 +165,7 @@ if (module === require.main) {
 	  console.log("¡Collección sensores creada exitosamente!");
 	  db.close();
 	});
-	});
+  });
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
 		var dbo = db.db("piCar");
@@ -92,7 +174,25 @@ if (module === require.main) {
 		console.log("¡Collección movimientos creada exitosamente!");
 		db.close();
 		});
+  });
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("piCar");
+		dbo.createCollection("estado", function(err, res) {
+		  if (err) throw err;
+		  console.log("¡Collección estado creada exitosamente!");
+		  db.close();
 	});
+  });
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("piCar");
+		dbo.createCollection("encendido", function(err, res) {
+		  if (err) throw err;
+		  console.log("¡Collección encendido creada exitosamente!");
+		  db.close();
+	});
+  });
   // [END server]
 }
 
